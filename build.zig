394b6f825fab21bc24b59fd36142d3d633299f1c
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-22 14:33:00 -0400

    Allowed the project to use raylib.

--- build.zig
+++ build.zig
@@ -103,6 +103,11 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
+    exe.linkLibC();
+    exe.addLibraryPath(.{ .path = "deps/raylib/build/raylib" });
+    exe.addIncludePath(.{ .path = "deps/raylib/src" });
+    exe.linkSystemLibrary("raylib");
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
