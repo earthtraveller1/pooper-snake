133786f0a40f2800102ccc210b9770b06f598c90
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-22 14:35:33 -0400

    Created a basic raylib program.

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,20 @@
 const std = @import("std");
+const raylib = @cImport(@cInclude("raylib.h"));
+
+const window_width = 800;
+const window_height = 600;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    raylib.InitWindow(window_width, window_height, "Pooper Snake");
+    defer raylib.CloseWindow();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    raylib.SetTargetFPS(60);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    while (!raylib.WindowShouldClose()) {
+        raylib.BeginDrawing();
+        raylib.ClearBackground(raylib.BLACK);
+        raylib.EndDrawing();
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+        raylib.PollInputEvents();
+    }
 }
