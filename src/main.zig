14462d24794232a91d2a670cf3194d98ea6cd831
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-22 19:47:58 -0400

    Tried to add a trail (but failed miserably).

--- src/main.zig
+++ src/main.zig
@@ -10,9 +10,15 @@ const assets = struct {
 const PlayerPart = struct {
     position: raylib.Vector2,
     direction: Direction,
+    next_direction: Direction,
     offset: f32,
     unit_x: f32,
     unit_y: f32,
+};
+
+const Rotation = struct {
+    new_direction: Direction,
+    timestamp: f64,
 };
 
 // Measured in pixel.
@@ -28,6 +34,8 @@ const player_speed = 6;
 const Direction = enum { left, right, up, down };
 
 pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
     raylib.InitWindow(window_width, window_height, "Pooper Snake");
     defer raylib.CloseWindow();
 
@@ -61,65 +69,94 @@ pub fn main() !void {
     raylib.UnloadTexture(crate_texture);
 
     var delta_time: f64 = 0;
-    var player_head: PlayerPart = .{
+
+    var player_parts = std.ArrayList(PlayerPart).init(allocator);
+    try player_parts.append(.{
         .unit_x = 0.0,
         .unit_y = 0.0,
         .offset = 0.0,
         .position = .{ .x = 0.0, .y = 0.0 },
         .direction = Direction.down,
-    };
+        .next_direction = Direction.down,
+    });
+    try player_parts.append(.{
+        .unit_x = 0.0,
+        .unit_y = 1.0,
+        .offset = 1.0 / @as(f32, player_speed),
+        .position = .{ .x = 0.0, .y = 0.0 },
+        .direction = Direction.down,
+        .next_direction = Direction.down,
+    });
 
-    var next_direction: Direction = Direction.down;
+    var rotations = std.ArrayList(Rotation).init(allocator);
+
+    const player_head = &player_parts.items[0];
 
     while (!raylib.WindowShouldClose()) {
         const start_time = raylib.GetTime();
 
         if (raylib.IsKeyPressed(raylib.KEY_UP)) {
-            next_direction = Direction.up;
+            player_head.*.next_direction = Direction.up;
+            try rotations.append(.{ .new_direction = player_head.*.next_direction, .timestamp = raylib.GetTime() });
         }
         if (raylib.IsKeyPressed(raylib.KEY_DOWN)) {
-            next_direction = Direction.down;
+            player_head.*.next_direction = Direction.down;
+            try rotations.append(.{ .new_direction = player_head.*.next_direction, .timestamp = raylib.GetTime() });
         }
         if (raylib.IsKeyPressed(raylib.KEY_RIGHT)) {
-            next_direction = Direction.right;
+            player_head.*.next_direction = Direction.right;
+            try rotations.append(.{ .new_direction = player_head.*.next_direction, .timestamp = raylib.GetTime() });
         }
         if (raylib.IsKeyPressed(raylib.KEY_LEFT)) {
-            next_direction = Direction.left;
+            player_head.*.next_direction = Direction.left;
+            try rotations.append(.{ .new_direction = player_head.*.next_direction, .timestamp = raylib.GetTime() });
         }
 
         const threshold = 0.1;
 
-        if (next_direction != player_head.direction) {
-            if (next_direction == Direction.up or next_direction == Direction.down) {
-                if (1.0 - (player_head.unit_x - std.math.floor(player_head.unit_x)) < threshold) {
-                    player_head.direction = next_direction;
-                    player_head.position.x = std.math.round(player_head.unit_x) * unit_size;
-                }
-            } else if (next_direction == Direction.left or next_direction == Direction.right) {
-                if (1.0 - (player_head.unit_y - std.math.floor(player_head.unit_y)) < threshold) {
-                    player_head.direction = next_direction;
-                    player_head.position.y = std.math.round(player_head.unit_y) * unit_size;
+        for (player_parts.items) |*player_part| {
+            if (rotations.items.len >= 1) {
+                var i: i32 = @intCast(rotations.items.len - 1);
+                while (i >= 0) : (i -= 1) {
+                    if (rotations.items[@intCast(i)].timestamp + player_part.offset < raylib.GetTime()) {
+                        player_part.next_direction = rotations.items[@intCast(i)].new_direction;
+                        break;
+                    }
                 }
             }
-        }
 
-        switch (player_head.direction) {
-            Direction.left => {
-                player_head.position.x -= @floatCast(player_speed * unit_size * delta_time);
-                player_head.unit_x -= @floatCast(player_speed * delta_time);
-            },
-            Direction.right => {
-                player_head.position.x += @floatCast(player_speed * unit_size * delta_time);
-                player_head.unit_x += @floatCast(player_speed * delta_time);
-            },
-            Direction.up => {
-                player_head.position.y -= @floatCast(player_speed * unit_size * delta_time);
-                player_head.unit_y -= @floatCast(player_speed * delta_time);
-            },
-            Direction.down => {
-                player_head.position.y += @floatCast(player_speed * unit_size * delta_time);
-                player_head.unit_y += @floatCast(player_speed * delta_time);
-            },
+            if (player_part.next_direction != player_part.direction) {
+                if (player_part.next_direction == Direction.up or player_part.next_direction == Direction.down) {
+                    if (1.0 - (player_part.unit_x - std.math.floor(player_head.unit_x)) < threshold) {
+                        player_part.direction = player_part.next_direction;
+                        player_part.position.x = std.math.round(player_head.unit_x) * unit_size;
+                    }
+                } else if (player_part.next_direction == Direction.left or player_part.next_direction == Direction.right) {
+                    if (1.0 - (player_part.unit_y - std.math.floor(player_head.unit_y)) < threshold) {
+                        player_part.direction = player_part.next_direction;
+                        player_part.position.y = std.math.round(player_head.unit_y) * unit_size;
+                    }
+                }
+            }
+
+            switch (player_part.direction) {
+                Direction.left => {
+                    player_part.position.x -= @floatCast(player_speed * unit_size * delta_time);
+                    player_part.unit_x -= @floatCast(player_speed * delta_time);
+                },
+                Direction.right => {
+                    player_part.position.x += @floatCast(player_speed * unit_size * delta_time);
+                    player_part.unit_x += @floatCast(player_speed * delta_time);
+                },
+                Direction.up => {
+                    player_part.position.y -= @floatCast(player_speed * unit_size * delta_time);
+                    player_part.unit_y -= @floatCast(player_speed * delta_time);
+                },
+                Direction.down => {
+                    player_part.position.y += @floatCast(player_speed * unit_size * delta_time);
+                    player_part.unit_y += @floatCast(player_speed * delta_time);
+                },
+            }
         }
 
         raylib.BeginDrawing();
@@ -129,7 +166,10 @@ pub fn main() !void {
         // Tile the background.
         raylib.DrawTexture(crate_background.texture, 0, 0, raylib.WHITE);
 
-        raylib.DrawTextureV(can_pooper_texture, player_head.position, raylib.WHITE);
+        for (player_parts.items) |part| {
+            raylib.DrawTextureV(can_pooper_texture, part.position, raylib.WHITE);
+        }
+
         raylib.DrawTexture(burger_texture, 100, 100, raylib.WHITE);
 
         raylib.EndDrawing();
