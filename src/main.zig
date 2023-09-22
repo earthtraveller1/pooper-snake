dfca556836aa1fdc8f574c5290773153c86584fc
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-22 18:50:13 -0400

    Created a playerpart struct.

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,13 @@ const assets = struct {
     const crate_png = @embedFile("assets/crate.png");
 };
 
+const PlayerPart = struct {
+    position: raylib.Vector2,
+    direction: Direction,
+    unit_x: f32,
+    unit_y: f32,
+};
+
 // Measured in pixel.
 const window_width = 800;
 const window_height = 600;
@@ -48,12 +55,13 @@ pub fn main() !void {
     }
 
     var delta_time: f64 = 0;
-    var player_position: raylib.Vector2 = .{ .x = 0.0, .y = 0.0 };
+    var player_head: PlayerPart = .{
+        .unit_x = 0.0,
+        .unit_y = 0.0,
+        .position = .{ .x = 0.0, .y = 0.0 },
+        .direction = Direction.down,
+    };
 
-    var player_x_unit: f32 = 0.0;
-    var player_y_unit: f32 = 0.0;
-
-    var player_direction: Direction = Direction.down;
     var next_direction: Direction = Direction.down;
 
     while (!raylib.WindowShouldClose()) {
@@ -74,36 +82,36 @@ pub fn main() !void {
 
         const threshold = 0.1;
 
-        if (next_direction != player_direction) {
+        if (next_direction != player_head.direction) {
             if (next_direction == Direction.up or next_direction == Direction.down) {
-                if (1.0 - (player_x_unit - std.math.floor(player_x_unit)) < threshold) {
-                    player_direction = next_direction;
-                    player_position.x = std.math.round(player_x_unit) * unit_size;
+                if (1.0 - (player_head.unit_x - std.math.floor(player_head.unit_x)) < threshold) {
+                    player_head.direction = next_direction;
+                    player_head.position.x = std.math.round(player_head.unit_x) * unit_size;
                 }
             } else if (next_direction == Direction.left or next_direction == Direction.right) {
-                if (1.0 - (player_y_unit - std.math.floor(player_y_unit)) < threshold) {
-                    player_direction = next_direction;
-                    player_position.y = std.math.round(player_y_unit) * unit_size;
+                if (1.0 - (player_head.unit_y - std.math.floor(player_head.unit_y)) < threshold) {
+                    player_head.direction = next_direction;
+                    player_head.position.y = std.math.round(player_head.unit_y) * unit_size;
                 }
             }
         }
 
-        switch (player_direction) {
+        switch (player_head.direction) {
             Direction.left => {
-                player_position.x -= @floatCast(player_speed * unit_size * delta_time);
-                player_x_unit -= @floatCast(player_speed * delta_time);
+                player_head.position.x -= @floatCast(player_speed * unit_size * delta_time);
+                player_head.unit_x -= @floatCast(player_speed * delta_time);
             },
             Direction.right => {
-                player_position.x += @floatCast(player_speed * unit_size * delta_time);
-                player_x_unit += @floatCast(player_speed * delta_time);
+                player_head.position.x += @floatCast(player_speed * unit_size * delta_time);
+                player_head.unit_x += @floatCast(player_speed * delta_time);
             },
             Direction.up => {
-                player_position.y -= @floatCast(player_speed * unit_size * delta_time);
-                player_y_unit -= @floatCast(player_speed * delta_time);
+                player_head.position.y -= @floatCast(player_speed * unit_size * delta_time);
+                player_head.unit_y -= @floatCast(player_speed * delta_time);
             },
             Direction.down => {
-                player_position.y += @floatCast(player_speed * unit_size * delta_time);
-                player_y_unit += @floatCast(player_speed * delta_time);
+                player_head.position.y += @floatCast(player_speed * unit_size * delta_time);
+                player_head.unit_y += @floatCast(player_speed * delta_time);
             },
         }
 
@@ -114,7 +122,7 @@ pub fn main() !void {
         // Tile the background.
         raylib.DrawTexture(crate_background.texture, 0, 0, raylib.WHITE);
 
-        raylib.DrawTextureV(can_pooper_texture, player_position, raylib.WHITE);
+        raylib.DrawTextureV(can_pooper_texture, player_head.position, raylib.WHITE);
         raylib.DrawTexture(burger_texture, 100, 100, raylib.WHITE);
 
         raylib.EndDrawing();
