00900f7746445da4895d0edfefb532795d2020c7
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-22 15:22:08 -0400

    Created a system for moving the player.

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,15 @@ const assets = struct {
     const burger_png = @embedFile("assets/burger.png");
 };
 
+// Measured in pixel.
 const window_width = 800;
 const window_height = 600;
+
+// Measured in pixels.
+const unit_size = 50;
+
+// Measured in units per second.
+const player_speed = 3;
 
 pub fn main() !void {
     raylib.InitWindow(window_width, window_height, "Pooper Snake");
@@ -21,15 +28,25 @@ pub fn main() !void {
     const burger_image = raylib.LoadImageFromMemory(".png", assets.burger_png, assets.burger_png.len);
     const burger_texture = raylib.LoadTextureFromImage(burger_image);
 
+    var delta_time: f64 = 0;
+    var player_position: raylib.Vector2 = .{ .x = 0.0, .y = 0.0 };
+
     while (!raylib.WindowShouldClose()) {
+        const start_time = raylib.GetTime();
+
+        player_position.y += @floatCast(player_speed * unit_size * delta_time);
+
         raylib.BeginDrawing();
 
         raylib.ClearBackground(raylib.BLACK);
-        raylib.DrawTexture(can_pooper_texture, 10, 10, raylib.WHITE);
+        raylib.DrawTextureV(can_pooper_texture, player_position, raylib.WHITE);
         raylib.DrawTexture(burger_texture, 100, 100, raylib.WHITE);
 
         raylib.EndDrawing();
 
         raylib.PollInputEvents();
+
+        const end_time = raylib.GetTime();
+        delta_time = end_time - start_time;
     }
 }
