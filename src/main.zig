87a1db05f37f1ca52a642c4fb833bfc86bdb3a54
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-22 15:07:47 -0400

    Tested drawing can pooper to the screen.

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 const raylib = @cImport(@cInclude("raylib.h"));
 
+const assets = struct {
+    const can_pooper_right_png = @embedFile("assets/canpooper.png");
+};
+
 const window_width = 800;
 const window_height = 600;
 
@@ -10,9 +14,15 @@ pub fn main() !void {
 
     raylib.SetTargetFPS(60);
 
+    const can_pooper_image = raylib.LoadImageFromMemory(".png", assets.can_pooper_right_png, assets.can_pooper_right_png.len);
+    const can_pooper_texture = raylib.LoadTextureFromImage(can_pooper_image);
+
     while (!raylib.WindowShouldClose()) {
         raylib.BeginDrawing();
+
         raylib.ClearBackground(raylib.BLACK);
+        raylib.DrawTexture(can_pooper_texture, 10, 10, raylib.WHITE);
+
         raylib.EndDrawing();
 
         raylib.PollInputEvents();
