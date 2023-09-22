723c4ad80789a59d1257e0491315a2b0c5b30e2e
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-22 15:12:33 -0400

    Drew the burger onto the screen as well.

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,8 @@ const std = @import("std");
 const raylib = @cImport(@cInclude("raylib.h"));
 
 const assets = struct {
-    const can_pooper_right_png = @embedFile("assets/canpooper.png");
+    const canpooper_png = @embedFile("assets/canpooper.png");
+    const burger_png = @embedFile("assets/burger.png");
 };
 
 const window_width = 800;
@@ -14,14 +15,18 @@ pub fn main() !void {
 
     raylib.SetTargetFPS(60);
 
-    const can_pooper_image = raylib.LoadImageFromMemory(".png", assets.can_pooper_right_png, assets.can_pooper_right_png.len);
+    const can_pooper_image = raylib.LoadImageFromMemory(".png", assets.canpooper_png, assets.canpooper_png.len);
     const can_pooper_texture = raylib.LoadTextureFromImage(can_pooper_image);
+
+    const burger_image = raylib.LoadImageFromMemory(".png", assets.burger_png, assets.burger_png.len);
+    const burger_texture = raylib.LoadTextureFromImage(burger_image);
 
     while (!raylib.WindowShouldClose()) {
         raylib.BeginDrawing();
 
         raylib.ClearBackground(raylib.BLACK);
         raylib.DrawTexture(can_pooper_texture, 10, 10, raylib.WHITE);
+        raylib.DrawTexture(burger_texture, 100, 100, raylib.WHITE);
 
         raylib.EndDrawing();
 
